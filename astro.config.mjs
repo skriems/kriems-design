c23cf97841a8b1de273b609262170f3ccf56e455
Sebastian Kriems <bastoberto@gmx.de>
2024-08-16 23:11:16 +0200

    chore: specify correct site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://skriems@github.io",
-  base: "kriems-design",
+  site: "https://www.kriems.design",
 });
